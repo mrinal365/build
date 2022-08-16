f1f81bce539b2864c61fefb1241bcdbcb25a0219
mrinal365 <40198111+mrinal365@users.noreply.github.com>
2022-08-16 11:20:30 +0530

    Add files via upload

---
+++ precache-manifest.0a32a6d472351bfa46d8731c485c2616.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2092f9433e87ad3f9db625c6f7bbd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10e6024b529e21e0d7b4",
+    "url": "/static/css/3.910877e1.chunk.css"
+  },
+  {
+    "revision": "23f932da4194f34ef9b0",
+    "url": "/static/css/5.910877e1.chunk.css"
+  },
+  {
+    "revision": "ef0049299126dc64873f",
+    "url": "/static/css/main.079acfe3.chunk.css"
+  },
+  {
+    "revision": "18d3a31cc72a48e416fc",
+    "url": "/static/js/0.51a69344.chunk.js"
+  },
+  {
+    "revision": "10e6024b529e21e0d7b4",
+    "url": "/static/js/3.aee4ee4d.chunk.js"
+  },
+  {
+    "revision": "9b94363a3eb43f14f6a5",
+    "url": "/static/js/4.00b5fb55.chunk.js"
+  },
+  {
+    "revision": "23f932da4194f34ef9b0",
+    "url": "/static/js/5.e0ff3517.chunk.js"
+  },
+  {
+    "revision": "64b155831b69f63375ca",
+    "url": "/static/js/6.d97452b0.chunk.js"
+  },
+  {
+    "revision": "4d072aa61c94e64c3cf1",
+    "url": "/static/js/7.f886d30a.chunk.js"
+  },
+  {
+    "revision": "8df2545a52275f98c3b6",
+    "url": "/static/js/8.fe2fee6b.chunk.js"
+  },
+  {
+    "revision": "ef0049299126dc64873f",
+    "url": "/static/js/main.9ae1e08b.chunk.js"
+  },
+  {
+    "revision": "dde9f5390f3d93acf650",
+    "url": "/static/js/runtime~main.a9c27fa0.js"
+  },
+  {
+    "revision": "af3bbf9c69f7705aff991f2b1a75d8be",
+    "url": "/static/media/3Oeayev6_400x400.af3bbf9c.jpg"
+  },
+  {
+    "revision": "663880fad3f162367bea3a979d0a88a2",
+    "url": "/static/media/Ep0OwpJM_400x400.663880fa.jpg"
+  },
+  {
+    "revision": "6ba88f897097f62e199d723c991f0b0d",
+    "url": "/static/media/JtpX95Rt_400x400.6ba88f89.jpg"
+  },
+  {
+    "revision": "7f26e1a2dcc7603b31954afeed40a865",
+    "url": "/static/media/TIktok.7f26e1a2.png"
+  },
+  {
+    "revision": "b144276067a42d67f756a81b0aab7e7f",
+    "url": "/static/media/bsclogo.b1442760.png"
+  },
+  {
+    "revision": "3fae4b9b79fb12edca5a4f2a15a8a2a9",
+    "url": "/static/media/chainlink.3fae4b9b.jpg"
+  },
+  {
+    "revision": "0d9dd04d1885d3d5c1ba1c3d976cc50a",
+    "url": "/static/media/dTKRnmIp_400x400.0d9dd04d.jpg"
+  },
+  {
+    "revision": "bd5b561c679073582472169bdfee5a31",
+    "url": "/static/media/logo_meta.72fa6a71.bd5b561c.png"
+  },
+  {
+    "revision": "72fa6a7125b14c0aac738afb01caeed5",
+    "url": "/static/media/logo_meta.72fa6a71.png"
+  },
+  {
+    "revision": "03cf269ff3342b8f651cfd8704f38060",
+    "url": "/static/media/maincard.03cf269f.gif"
+  },
+  {
+    "revision": "2d0a0d8f5f173be15a67aa084db94fe6",
+    "url": "/static/media/materialdesignicons-webfont.2d0a0d8f.eot"
+  },
+  {
+    "revision": "b4917be25082eb793b5363f2fdb5f282",
+    "url": "/static/media/materialdesignicons-webfont.b4917be2.woff"
+  },
+  {
+    "revision": "d0066537ab6a4c6f8285a5aeb3ba5f09",
+    "url": "/static/media/materialdesignicons-webfont.d0066537.woff2"
+  },
+  {
+    "revision": "f51112347be6b44f9ef46151a971430d",
+    "url": "/static/media/materialdesignicons-webfont.f5111234.ttf"
+  },
+  {
+    "revision": "023762b6aec2a2249b8fdfb638f00ef3",
+    "url": "/static/media/metamask.023762b6.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "8215855c185176eb79446ce8cc1f3998",
+    "url": "/static/media/walletConnectIcon.8215855c.svg"
+  },
+  {
+    "revision": "f0b227c86ea2b610bbbf131dbea3691a",
+    "url": "/static/media/x.f0b227c8.svg"
+  }
+]);
---
+++ static/js/8.fe2fee6b.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpMetaStocksUI=window.webpackJsonpMetaStocksUI||[]).push([[8],{758:function(o,n){}}]);
+//# sourceMappingURL=8.fe2fee6b.chunk.js.map
